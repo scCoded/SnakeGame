d0ee64f6d7f41090a6a87dce34a40d27ca3cd096
Sophie <clark.sophie2000@gmail.com>
2022-09-04 19:07:09 +0100

    build files

---
+++ build/precache-manifest.025a953e80fd7e42b63c31062316efdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d241bfc12c509ef9bf0c9044669c4b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe0cfa9f5fd2ae55c99",
+    "url": "/static/css/main.4ddb26ab.chunk.css"
+  },
+  {
+    "revision": "c9b7080935cd493623ae",
+    "url": "/static/js/2.52f5a7d3.chunk.js"
+  },
+  {
+    "revision": "bbe0cfa9f5fd2ae55c99",
+    "url": "/static/js/main.9e386009.chunk.js"
+  },
+  {
+    "revision": "9fd0aaebf8217be005f5",
+    "url": "/static/js/runtime-main.8c64e02a.js"
+  }
+]);
